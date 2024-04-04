5e2261ecbdc66bd1ac928d4b0317752ef15a7f76
Erdinc Mutlu <erdincmutlu@gmail.com>
2024-04-04 22:10:46 +0100

    Trying module in application

--- toolkit/tools.go
+++ toolkit/tools.go
@@ -4,8 +4,12 @@ import "crypto/rand"
 
 const randomStringSource = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_+"
 
+// Tools is the type used to instantiate this module. Any variableof this type
+// will access all the methods with the receiver *Tools
 type Tools struct{}
 
+// RandomString returns a string of random characters of length n, using
+// randomStringSource as the source for the string
 func (t *Tools) RandomString(n int) string {
 	s := make([]rune, n)
 	r := []rune(randomStringSource)
