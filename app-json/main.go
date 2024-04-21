fbafee33a7ec25c12fd9827d5b1abaf01f17e9d9
Erdinc Mutlu <erdincmutlu@gmail.com>
2024-04-21 21:10:01 +0100

    Web app for JSON

---
+++ app-json/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+type RequestPayload struct {
+	Action  string `json:"action"`
+	Message string `json:"message"`
+}
+
+type ResponsePayload struct {
+	Message    string `json:"message"`
+	StatusCode int    `json:"status_code,omitempty"`
+}
+
+func main() {
+	// create a default serve mux
+	mux := http.NewServeMux()
+
+	// register routes
+	mux.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/receive-post", receivePost)
+	mux.HandleFunc("/remote-service", remoteService)
+
+	// print a message
+	log.Println("Starting service...")
+
+	// start server
+	err := http.ListenAndServe(":8081", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func receivePost(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func remoteService(w http.ResponseWriter, r *http.Request) {
+
+}
